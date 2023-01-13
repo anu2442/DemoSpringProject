1401b6f2bd3053ca2333e9d031feee8e579802bc
anu2422 <singhanurag244275@gmail.com>
2023-01-13 18:23:31 +0530

    added database: connected with mysql database

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.entities.Course;
 import com.example.demo.services.CourseService;
+import com.example.demo.services.CourseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,12 +15,6 @@ public class MyController {
 
     @Autowired
     private CourseService courseService;
-
-    @GetMapping("/home")
-    public String home(){
-        return "L babu chimtu";
-
-    }
 
     // get the courses
     @GetMapping("/courses")
@@ -37,7 +34,7 @@ public class MyController {
         return this.courseService.addCourse(course);
     }
 
-    // put the course
+    // update the course
     @PutMapping("/courses")
     public Course putCourse(@RequestBody Course course){
         return this.courseService.putCourse(course);
@@ -45,8 +42,12 @@ public class MyController {
 
     // delete the course
     @DeleteMapping("/courses/{courseId}")
-    public void deleteCourse(@PathVariable String courseId){
-        this.courseService.deleteCourse(Long.parseLong(courseId));
-        return;
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId){
+        try {
+            this.courseService.deleteCourse(Long.parseLong(courseId));
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/example/demo/services/CourseServiceImpl.java
+++ src/main/java/com/example/demo/services/CourseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.services;
 
+import com.example.demo.dao.CourseDao;
 import com.example.demo.entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,61 +11,81 @@ import java.util.List;
 @Service
 public class CourseServiceImpl implements CourseService{
 
-    List<Course> list;
+//    List<Course> list;
+    @Autowired
+    private CourseDao courseDao;
 
     public CourseServiceImpl() {
 
-        list = new ArrayList<>();
-        list.add(new Course(145,"java","this is basics of java"));
-        list.add(new Course(4343,"Spring java","this is basics of spring java"));
+//        list = new ArrayList<>();
+//        list.add(new Course(145,"java","this is basics of java"));
+//        list.add(new Course(4343,"Spring java","this is basics of spring java"));
     }
 
     @Override
     public List<Course> getCourses() {
-        return list;
+
+//        return list;
+        return courseDao.findAll();
     }
 
     @Override
     public Course getCourse(long courseId) {
 
-        Course course = null;
-        for (Course c:list) {
-            if (c.getId() == courseId){
-                course = c;
-                break;
+//        Course course = null;
+//        for (Course c:list) {
+//            if (c.getId() == courseId){
+//                course = c;
+//                break;
+//            }
+//        }
+//        return course;
+//        return courseDao.getOne(courseId);
+        List<Course> temp = courseDao.findAll();
+        for (Course c :
+                temp) {
+            if (c.getId() == courseId) {
+                return c;
             }
-        }
-        return course;
+            }
+        return new Course();
     }
 
     @Override
     public Course addCourse(Course course) {
-        list.add(course);
+//        list.add(course);
+//        return course;
+
+        courseDao.save(course);
         return course;
     }
 
     @Override
     public Course putCourse(Course course) {
 
-        for (Course c:list){
-            if (c.getId() == course.getId()){
-                list.remove(c);
-                list.add(course);
-                break;
-            }
-        }
+//        for (Course c:list){
+//            if (c.getId() == course.getId()){
+//                list.remove(c);
+//                list.add(course);
+//                break;
+//            }
+//        }
+//        return course;
+
+        courseDao.save(course);
         return course;
     }
 
     @Override
     public void deleteCourse(long courseId) {
 
-        for (Course course:list){
-            if (course.getId() == courseId){
-                list.remove(course);
-                break;
-            }
-        }
-        return;
+//        for (Course course:list){
+//            if (course.getId() == courseId){
+//                list.remove(course);
+//                break;
+//            }
+//        }
+
+        courseDao.delete(courseDao.getOne(courseId));
     }
 }
